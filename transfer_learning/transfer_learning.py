b8a347cc68d690aa93768fee952b2cccca1bdb14
fangangping <906370564@qq.com>
2018-07-03 16:56:02 +0800

    fix some bugs

--- transfer_learning/transfer_learning.py
+++ transfer_learning/transfer_learning.py
@@ -1,4 +1,3 @@
-
 import torch
 from torch import nn
 from torch import optim
@@ -35,6 +34,8 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def train_model(model, criterion, optimizer, train_dataloader, val_dataloader, num_epochs=25):
 
+    model.to(device)
+
     best_model = copy.deepcopy(model.state_dict())
     best_acc = 0
 
@@ -47,6 +48,8 @@ def train_model(model, criterion, optimizer, train_dataloader, val_dataloader, n
         train_corrects = 0
         for data, labels in train_dataloader:
             #forward pass
+            data.to(device)
+            labels.to(device)
             optimizer.zero_grad()
             outputs = model(data)
             loss = criterion(outputs, labels)
@@ -58,9 +61,10 @@ def train_model(model, criterion, optimizer, train_dataloader, val_dataloader, n
             #backwar pass and update network
             loss.backward()
             optimizer.step()
-            train_loss += loss.item()
+            train_loss += loss.item() * data.size(0)
             train_corrects += torch.sum(preds == labels.data).item()
 
+        train_loss /= len(train_dataloader.dataset)
         train_acc = train_corrects / len(train_dataloader.dataset)
         print('epoch {} train loss {} correct {}'.format(epoch, train_loss, train_acc))
 
@@ -69,18 +73,21 @@ def train_model(model, criterion, optimizer, train_dataloader, val_dataloader, n
         val_loss = 0
         val_corrects = 0
         with torch.no_grad():
-            for data, label in val_dataloader:
+            for data, labels in val_dataloader:
                 #forward pass
+                data.to(device)
+                labels.to(device)
                 outputs = model(data)
-                loss = criterion(outputs, label)
+                loss = criterion(outputs, labels)
 
                 #loss and predict
                 _, preds = torch.max(outputs, 1)
-                val_loss += loss.item()
+                val_loss += loss.item() * data.size(0)
                 val_corrects += torch.sum(preds == labels.data).item()
 
-
+        val_loss /= len(val_dataloader.dataset)
         val_acc = val_corrects / len(val_dataloader.dataset)
+
         if val_acc > best_acc:
             best_acc = val_acc
             best_model = model.state_dict()
@@ -91,18 +98,17 @@ def train_model(model, criterion, optimizer, train_dataloader, val_dataloader, n
 
     return best_model
 
+
 model_conv = torchvision.models.resnet18(pretrained=True)
 for param in model_conv.parameters():
     param.requires_grad = False
 
 
 num_features = model_conv.fc.in_features
-print(model_conv.fc.in_features)
 model_conv.fc = nn.Linear(num_features, 2)
 
-optimizer_conv = optim.Adam(model_conv.fc.parameters(), lr=1e-3)
+optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=1e-3, momentum=0.9)
 
 criterion = nn.CrossEntropyLoss()
 
 train_model(model_conv, criterion, optimizer_conv,train_dataloader, val_dataloader)
-

4d139566c9931766ec6a553a713fa04d8f424aaa
fangangping <906370564@qq.com>
2018-07-04 10:44:57 +0800

    finish reading data

--- RNN/ClassifyName.py
+++ RNN/ClassifyName.py
@@ -5,34 +5,45 @@ from torch import nn
 from torch.utils import  data
 
 all_letters = 'PFGì-BÁ òŚ/ŻATowIèáJtńybiWVsMEUüZCHÉùra,c:YXäDnçfOugêRKzelúãjvõxSpLQßàó1íNñłżédąhmkq ö\''
+all_countries = [
+    'Arabic', 'English', 'Irish', 'Polish', 'Spanish', 'Chinese', 'French', 'Italian', 'Portuguese',
+    'Vietnamese', 'Czech', 'German', 'Japanese', 'Russian','Dutch', 'Greek', 'Korean', 'Scottish'
+    ]
 
-class Name(data.Dataset):
+class NameData(data.Dataset):
+
 
     def __init__(self, root):
-        super(Name, self).__init__()
-        self
-
+        super(NameData, self).__init__()
+        self.data = []
+        self.labels = []
+        print('init')
         for dir in os.listdir(root):
+            #read label from txtname
             label = dir.split('.')[0]
-            data[label] = []
-            # read txt
             with open(os.path.join(root, dir), 'r') as f:
+                # read all name in each text
                 for line in f.readlines():
                     name = line.rstrip('\n')
+                    name_tensor = torch.zeros(name.__len__(), all_letters.__len__())
+                    # name to one hot tensor
                     for i, ch in enumerate(name):
                         index = all_letters.find(ch)
-                        if index < -1:
-                         ta
-
+                        if index < 0:
+                            raise RuntimeError('unsupported character')
                         name_tensor[i, index] = 1
-
+                    self.data.append(name_tensor)
+                    self.labels.append(all_countries.index(label))
+        print('init finish')
+        for i in self.data:
+            print(i)
 
 
     def __len__(self):
-        pass
+        return self.data.__len__()
 
     def __getitem__(self, index):
-        pass
+        return self.data[index], self.labels[index]
 
 
 class RNN(nn.Module):
@@ -43,17 +54,4 @@ class RNN(nn.Module):
     def forward(self, *input):
         pass
 
-data = {}
-root = './data/names/'
-
-def name_to_tensor(name):
-    name_tensor = torch.zeros(name.__len__(), all_letters.__len__())
-    for i ,ch in enumerate(name):
-        index = all_letters.find(ch)
-        if index < -1:
-            print('dasdsadsadsa')
-        name_tensor[i ,index] = 1
-
-for country, names in data.items():
-    for name in names:
-        name_to_tensor(name)
+a = NameData('data/names')
